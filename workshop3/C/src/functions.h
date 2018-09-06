f2e05b4d5fbb1fad27db2a0234c87145bc883995
taataam <taher.chegini@gmail.com>
2018-09-05 19:04:34 -0500

    new openMP lops

--- workshop3/C/src/functions.h
+++ workshop3/C/src/functions.h
@@ -40,24 +40,19 @@ double **array_2d(const int row, const int col) {
 void phi_gc(double **phi, double **phi_x, double **phi_y, const int r[2][4]) {
   int i, j;
 
+  #pragma omp parallel for private(i,j) schedule(auto)
   for (i = r[0][0]; i < r[0][1]; i++) {
     for (j = r[0][2]; j < r[0][3]; j++) {
       phi_x[i][j] = (phi[i][j] + phi[i - 1][j]) * 0.5;
     }
   }
 
+  #pragma omp parallel for private(i,j) schedule(auto)
   for (i = r[1][0]; i < r[1][1]; i++) {
     for (j = r[1][2]; j < r[1][3]; j++) {
       phi_y[i][j] = (phi[i][j] + phi[i][j - 1]) * 0.5;
     }
   }
-}
-
-/* Calculate laplacian of a field at grid point i, j */
-double div2(double **phi, const int r, const int c,
-            const double dtdxx, const double dtdyy) {
-  return dtdxx * (phi[r + 1][c] - 2.0 * phi[r][c] + phi[r - 1][c])
-       + dtdyy * (phi[r][c + 1] - 2.0 * phi[r][c] + phi[r][c - 1]);
 }
 
 #endif /* FUNCTIONS_H */
